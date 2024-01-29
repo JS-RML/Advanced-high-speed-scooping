742fe98cbc02f70fceaabc5f8f63a7817d1baf43
chahyeonje <guswp3611@gmail.com>
2024-01-29 16:07:58 +0900

    Added controlling card code.

--- gripper/doControlCard.py
+++ gripper/doControlCard.py
@@ -1,14 +1,18 @@
-from . import mainGripper
-from .ScoopingObject import *
+from time import sleep
+from ScoopingObject import *
+from enums import MotorDriverSerialNumber
+from enums import FREQUANCY
 
 def doControlCard():
     count = 0
     print("[Case card is selected]")
-    card = ScoopingObject(mainGripper.SN_L0, mainGripper.SN_L1, mainGripper.SN_R0, mainGripper.SN_R1)
+    card = ScoopingObject(MotorDriverSerialNumber.L0, MotorDriverSerialNumber.L1, MotorDriverSerialNumber.R0, MotorDriverSerialNumber.R1)
     card.StandbyPosition(-0.051, 0.022, -0.057, 0.269)
     card.ScoopingPostion(0.026, 0.016, 0.076, 0.266)
     card.GrabPosition(0.032, 0.020, -0.152, 0.289)
 
-    while(count <200) :
+    while(count < 1):
+        print("Card...",count)
+        count += 1/FREQUANCY
+        sleep(1/FREQUANCY)
 
-        count += 1
