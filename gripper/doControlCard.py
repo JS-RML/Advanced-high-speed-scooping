c47e44f6408ff595e6fafd644ec63ea6e792ca73
chahyeonje <guswp3611@gmail.com>
2024-01-31 10:19:27 +0900

    Changed position parameters and codes for checking motor position.

--- gripper/doControlCard.py
+++ gripper/doControlCard.py
@@ -8,25 +8,53 @@ def doControlCard():
     print("[Case card is selected]")
     card = ScoopingObject(MotorDriverSerialNumber.L0.value, MotorDriverSerialNumber.L1.value, MotorDriverSerialNumber.R0.value, MotorDriverSerialNumber.R1.value)
     card.StandbyPosition = -0.089, 0.018, -0.044, 0.294
-    card.ScoopingPosition = 0.026, 0.016, 0.076, 0.226
-    card.GrabPosition = 0.032, 0.020, -0.152, 0.289
+    card.ScoopingPosition = 0.046, -0.04, 0.12, 0.176
+    card.GrabPosition = 0.046, -0.04, -0.136, 0.309
 
     # print(card.CurrentMotorEncoderValue)
 
     card.Move2StanbyPosition
+    print("stanby position")
     sleep(0.5)
-    # card.Move2ScoopingPosition
-    # sleep(0.5)
+    card.Move2ScoopingPosition
+    print("scooping position")
+    sleep(0.5)
     # card.Move2GrabPosition
+    # print("Grab position")
     # sleep(0.5)
     # card.Move2ScoopingPosition
+    tempEncoderVar = np.zeros(4)
+    prevtempEncoderVar = np.zeros(4)
+    encoderDifference = np.zeros(4)
+    firstRun = True
 
-    while(count < 5):
-        # print("Card...",count)
+    while(count < 3):
+
         # user_input = input("아무 키를 입력하시오")
         # if user_input:
+        #     card.SetIdleState()
         #     break
+
+        tempEncoderVar = card.CurrentMotorEncoderValue
+
+        if firstRun:
+            prevtempEncoderVar = tempEncoderVar
+            firstRun = False
+        else :
+            pass
+
+        encoderDifference = abs(tempEncoderVar - prevtempEncoderVar)
+
+        if encoderDifference[0] > 0.005 or encoderDifference[1] > 0.005 :
+            card.Move2GrabPosition
+        else :
+            pass
+
+        print(encoderDifference)
+
         count += 1/FREQUANCY
         sleep(1/FREQUANCY)
+        prevtempEncoderVar = tempEncoderVar
+
 
     card.SetIdleState()
