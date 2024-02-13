59dcbbcf69d6fa1007c298e152ced1ffae63657c
chahyeonje <guswp3611@gmail.com>
2024-02-13 20:07:50 +0900

    Added clean up code and changed path name.

--- RB5/mainRB5.py
+++ RB5/mainRB5.py
@@ -1,11 +1,17 @@
 from time import sleep
 import sys
 import os
-current_dir = os.path.dirname(os.path.abspath(__file__)) # 예를들어 부모 디렉토리를 만든다면 parent_dir = os.path.join(current_dir, '..') 이렇게도 가능
-sys.path.append(current_dir)
-from enums import FREQUANCY
+current_dir_rb = os.path.dirname(os.path.abspath(__file__)) # 예를들어 부모 디렉토리를 만든다면 parent_dir = os.path.join(current_dir, '..') 이렇게도 가능
+sys.path.append(current_dir_rb)
+# from enums import FREQUANCY
 import cobot
-
+import signal
+def cleanup_before_exit(signum, frame):
+    print("\nCtrl+Z detected. Exiting...")
+    cobot.MoveITPL_Clear()
+    cobot.MotionHalt()
+    cobot.DisConnectToCB()
+    # sys.exit(0)
 def disconnectngRB5():
     cobot.MoveITPL_Clear()
     cobot.MotionHalt()
@@ -18,21 +24,22 @@ def mainRB5():
     cobot.CobotInit()
     cobot.SetProgramMode(cobot.PG_MODE.REAL)
     cobot.MoveL(x=123.76, y=-448.94, z=304.20, rx=-139.6, ry=22.8, rz=67.3, spd=-1, acc=-1)
-    sleep(0.5)
-    cobot.MoveL(x=123.76, y=-448.94, z=403.26, rx=-139.6, ry=22.8, rz=67.3, spd=-1, acc=-1)
-    sleep(0.5)
-    cobot.MoveL(x=123.76, y=-448.94, z=304.20, rx=-139.6, ry=22.8, rz=67.3, spd=-1, acc=-1)
-    sleep(0.5)
-    cobot.MoveL(x=123.76, y=-448.94, z=403.26, rx=-139.6, ry=22.8, rz=67.3, spd=-1, acc=-1)
-    sleep(0.5)
-    cobot.MoveL(x=123.76, y=-448.94, z=304.20, rx=-139.6, ry=22.8, rz=67.3, spd=-1, acc=-1)
-    sleep(2.5)
-
-    disconnectngRB5()
-
+    # sleep(0.5)
+    # cobot.MoveL(x=123.76, y=-448.94, z=403.26, rx=-139.6, ry=22.8, rz=67.3, spd=-1, acc=-1)
+    # sleep(0.5)
+    # cobot.MoveL(x=123.76, y=-448.94, z=304.20, rx=-139.6, ry=22.8, rz=67.3, spd=-1, acc=-1)
+    # sleep(0.5)
+    # cobot.MoveL(x=123.76, y=-448.94, z=403.26, rx=-139.6, ry=22.8, rz=67.3, spd=-1, acc=-1)
+    # sleep(0.5)
+    # cobot.MoveL(x=123.76, y=-448.94, z=304.20, rx=-139.6, ry=22.8, rz=67.3, spd=-1, acc=-1)
+    # sleep(2.5)
+    #
+    # disconnectngRB5()
 
     # while(count <= 5):
     #     count += 1/FREQUANCY
     #     print("RB5...", count)
     #     sleep(1/FREQUANCY)
+
+    # signal.signal(signal.SIGTSTP, cleanup_before_exit)
 
--- gripper/mainGripper.py
+++ gripper/mainGripper.py
@@ -1,7 +1,7 @@
 import sys
 import os
-current_dir = os.path.dirname(os.path.abspath(__file__)) # 예를들어 부모 디렉토리를 만든다면 parent_dir = os.path.join(current_dir, '..') 이렇게도 가능
-sys.path.append(current_dir)
+current_dir_gripper = os.path.dirname(os.path.abspath(__file__)) # 예를들어 부모 디렉토리를 만든다면 parent_dir = os.path.join(current_dir, '..') 이렇게도 가능
+sys.path.append(current_dir_gripper)
 import doControlCard
 import doControlCracker
 import doContorlDomino
